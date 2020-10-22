6fc66c371e2556dd494e8729255ac74cc35feb61
Shirley <shiirley57000@gmail.com>
2020-10-22 21:48:00 +0200

    connerie de Dimitri

--- CESI/assets/app.js
+++ CESI/assets/app.js
@@ -9,6 +9,20 @@
 import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+// global.$ = global.jQuery = $;
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+
+
+$("#dim_button").on("click", function() {
+    let value = $("#dim_input").val();
+    if (value ==="dimxpas")
+    {
+        console.log(value);
+        $("#dim_secret").css("display", "block");
+        setInterval( function() {
+            $("#dim_secret").css("display", "none");
+        }, 3000);
+    }
+    console.log('Hello Webpack Encore! Edit me in assets/app.js');
+})
