f29a55fcc1c0941fed47e702ea04fde1ef3333aa
Shirley <shiirley57000@gmail.com>
2020-10-25 22:17:56 +0100

    test du repo

--- CESI/assets/app.js
+++ CESI/assets/app.js
@@ -10,19 +10,21 @@ import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
-// global.$ = global.jQuery = $;
 
 
-
+// CONNERIE DE DIMITRI
 $("#dim_button").on("click", function() {
     let value = $("#dim_input").val();
-    if (value ==="dimxpas")
+    if (value === "dimxpas")
     {
-        console.log(value);
-        $("#dim_secret").css("display", "block");
+        $("#dim_secret").removeClass("hidden");
         setInterval( function() {
-            $("#dim_secret").css("display", "none");
+            $("#dim_secret").addClass("hidden");
         }, 3000);
     }
+    $("#coucou").on("click", function() {
+$("#coucou").removeClass("show");
+$("#coucou").addClass("hidden");
+    })
     console.log('Hello Webpack Encore! Edit me in assets/app.js');
 })
