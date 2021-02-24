600a5168893822207990e3adc38f417920187a71
Swizz45 <maxma45000@gmail.com>
2021-02-24 19:33:28 +0100

    creation page + remise au propre de tt

---
+++ CESI/src/Controller/RessourcesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RessourcesController extends AbstractController
+{
+
+    /**
+     * @Route("/consultation-ressource", name="index")
+     */
+    public function index()
+    {
+        return $this->render('ressources/index.html.twig', [
+            'controller_name' => 'RessourcesController',
+        ]);
+    }
+    
+    /**
+     * @Route("/consultation-ressource/test", name="consultation-ressource")
+     */
+    public function consultation_ressources() {
+        return $this->render('ressources/_consultation-ressource.html.twig', [
+            'controller_name' => 'RessourcesController',
+        ]);
+    }
+}
